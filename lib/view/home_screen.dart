02aecadc0cbd5c047c69275ea7b23b1744989447
162504966+BlairyDev@users.noreply.github.com <162504966+FlowerKnight55@users.noreply.github.com>
2025-10-24 17:06:46 -0400

    fix Title becoming null

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -88,7 +88,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             item.attributes!.title!.es ??
                             item.attributes!.title!.esLa ??
                             item.attributes!.title!.koRo ??
-                            item.attributes!.title!.zhRo.toString(),
+                            item.attributes!.title!.koRo.toString(),
                         description: item.attributes!.description!.en
                             .toString(),
                         status: item.attributes!.status ?? "",
@@ -139,7 +139,14 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Text(
                             item.attributes!.title!.en ??
                                 item.attributes!.title!.jaRo ??
-                                item.attributes!.title!.ja.toString(),
+                                item.attributes!.title!.ja ??
+                                item.attributes!.title!.zh ??
+                                item.attributes!.title!.zhHk ??
+                                item.attributes!.title!.ptBr ??
+                                item.attributes!.title!.es ??
+                                item.attributes!.title!.esLa ??
+                                item.attributes!.title!.koRo ??
+                                item.attributes!.title!.koRo.toString(),
                             style: const TextStyle(
                               fontSize: 18.0,
                               color: Colors.white,
