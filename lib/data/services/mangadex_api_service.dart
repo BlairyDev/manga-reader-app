ff866a2ddfe794699f73ca4fcd89b7046b649637
162504966+BlairyDev@users.noreply.github.com <162504966+FlowerKnight55@users.noreply.github.com>
2025-11-01 10:07:47 -0400

    Fix issue with UI not displaying authors and artists

--- lib/data/services/mangadex_api_service.dart
+++ lib/data/services/mangadex_api_service.dart
@@ -42,7 +42,7 @@ class MangadexApiService {
     String text = Uri.encodeComponent(title);
 
     final String upcomingAPI =
-        "https://api.mangadex.org/manga?limit=6&offset=${offset}&title=${text}&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5BlatestUploadedChapter%5D=desc&includes%5B%5D=cover_art";
+        "https://api.mangadex.org/manga?limit=6&offset=${offset}&title=${text}&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5BlatestUploadedChapter%5D=desc&includes[]=artist&includes[]=author&includes[]=cover_art";
 
     return runAPI(upcomingAPI);
   }
